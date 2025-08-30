78f0ec9fa9da3da62c887a8cb93fbefcecc0118c
trev <me@trev.xyz>
2025-08-30 06:43:54 -0400

    bump: v0.1.7 -> v0.1.8

--- flake.nix
+++ flake.nix
@@ -99,7 +99,7 @@
       packages = with pkgs.nur.repos.trev.lib; rec {
         default = pkgs.buildGoModule (finalAttrs: {
           pname = "discord-embedder";
-          version = "0.1.7";
+          version = "0.1.8";
           src = ./.;
           goSum = ./go.sum;
           vendorHash = null;
