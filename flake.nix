c4550f1ac158213bfb67359bd7d598d1f5d84be2
spotdemo4 <me@trev.xyz>
2026-06-08 19:00:32 -0400

    chore(flake): bump package version to 0.1.21

--- flake.nix
+++ flake.nix
@@ -184,7 +184,7 @@
         packages.default = pkgs.buildGoModule (
           final: with pkgs.lib; {
             pname = "discord-embedder";
-            version = "0.1.20";
+            version = "0.1.21";
 
             src = fileset.toSource {
               root = ./.;
