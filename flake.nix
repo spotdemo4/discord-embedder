0326484da3a6e48850424a9705167af5f2126bce
doctor <doctor@trev.xyz>
2026-09-21 22:02:22 +0000

    bump: v0.5.0 -> v0.6.0

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
           default = pkgs.buildGoModule (
             final: with pkgs.lib; {
               pname = "discord-embedder";
-              version = "0.5.0";
+              version = "0.6.0";
               ldflags = [ "-X discord-embedder/internal/version.Application=${final.version}" ];
 
               src = fileset.toSource {
